fcd71de15c9d71797995b586b9e68663f39ffd88
Emilian Ognianov <emilian_89@abv.bg>
2019-12-12 02:39:08 +0200

    login and register pages added. input forms,validation

--- flask-blog.py
+++ flask-blog.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+from dotenv import load_dotenv
+import os
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistationForm, LoginForm
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 posts = [
     {
@@ -25,3 +32,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About us')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=='admin@admin.com' and form.password.data=="123":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Loggin failed!', 'danger')
+    return render_template('login.html', title='Login', form=form)
