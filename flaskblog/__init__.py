85bab0ae1ecf01bb9915c4e7ee163dfc00aa1b1b
Emilian Ognianov <emilian_89@abv.bg>
2019-12-13 15:09:45 +0200

    reset password functionallity

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 load_dotenv()
 
@@ -17,5 +18,15 @@ bcrypt = Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_SERVER'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+
+mail = Mail(app)
+
+
 
 from flaskblog import routes
