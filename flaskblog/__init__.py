0810b45287e6f9dbe8d9a95bf9d03dddba65bb19
eognianov <emilian_89@abv.bg>
2020-10-04 18:55:19 +0300

    update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -18,6 +19,7 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
